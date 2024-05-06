6296b830136000fa7a5b51ccec96d2edfe06678f
DevOluwatunmise <tunmisedare@gmail.com>
2024-05-06 16:56:37 +0100

    config

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes} from "react-router-dom";
 import AdminReg from "./Component/Register/AdminReg";
 import Login from "./Component/Register/Login";
 import StudentReg from "./Component/Register/StudentReg";
@@ -8,18 +8,33 @@ import StudentDashboard from "./Component/Dashboard/StudentDashboard";
 import Rooms from "./Component/Dashboard/Rooms";
 import AdminPreview from "./Component/AdminPreview/AdminPreview";
 import Attendance from "./Component/Attendance/Attendance";
+import { useState, useEffect } from "react";
+import Loader from "./Component/Loader";
+
+
 
 function App() {
-  return (
-    <>
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false)
+    },3000)
+  }, [])
+
+    const renderRoute = () => (
+  
       <div>
         <Routes>
           <Route path="/" element={<AdminReg />} />
 
           <Route path="/login" element={<Login />} />
           
-          <Route path="/student-reg" element={<StudentReg />} />
-
+          <Route path="/student-reg" element={
+          <Layout>
+          <StudentReg /> 
+          </Layout>
+          }/>
           <Route
             path="/homedash"
             element={
@@ -33,16 +48,19 @@ function App() {
           <Route path="/room" element={<Rooms />}/>
 
           <Route path="/adminsPrev" element={<AdminPreview />}/>
-          <Route path="/attendance" element={
-            
-            <Layout>
-          <Attendance />
+         <Route path="adttendance" element={
+
+         <Layout>
+         < Attendance/>
           </Layout>
-        } />
+        }/>
+        
         </Routes>
-      </div> 
-    </>
+        {/* </Layout> */}
+      </div>
+    
   );
+  return <>{loading ? <Loader/> : renderRoute()}</>
 }
 
 export default App;
